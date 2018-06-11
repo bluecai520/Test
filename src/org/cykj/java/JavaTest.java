e6a311d9f1cbd09cb07b57dc8d09fd692ad63d05
1152179588@qq.com <Viking@DESKTOP-USC05O6>
2018-06-11 16:23:42 +0800

    哈哈哈哈

--- src/org/cykj/java/JavaTest.java
+++ src/org/cykj/java/JavaTest.java
@@ -7,5 +7,7 @@ public class JavaTest {
 	public static void main(String args[]) {
 		System.out.println("hello world!");
 		System.out.println("我是范帅,我已经上传成功了，耶耶耶！！");
+		System.out.println("我是春哥,第一次修改测试代码");
+		
 	}
 }
