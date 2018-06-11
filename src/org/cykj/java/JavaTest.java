da143fba2f840d635f93524c2f6e0b6778862f9f
JX170922 <HASEE@DESKTOP-OO424HK>
2018-06-11 16:18:53 +0800

    毛聪 Test

--- src/org/cykj/java/JavaTest.java
+++ src/org/cykj/java/JavaTest.java
@@ -6,6 +6,8 @@ public class JavaTest {
 
 	public static void main(String args[]) {
 		System.out.println("hello world!");
-		System.out.println("我是范帅,我已经上传成功了，耶耶耶！！");
+		System.out.println("鎴戞槸鑼冨竻,鎴戝凡缁忎笂浼犳垚鍔熶簡锛岃�惰�惰�讹紒锛�");
+		System.out.println("Hello boys ,I am MaoCong");
+		
 	}
 }
