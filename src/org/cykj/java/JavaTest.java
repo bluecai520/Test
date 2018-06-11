b286c0338b7548172ce21121ba16b96cf80189e4
26446 <26446@ShuaiFan>
2018-06-11 12:02:55 +0800

    范帅的

--- src/org/cykj/java/JavaTest.java
+++ src/org/cykj/java/JavaTest.java
@@ -1,10 +1,11 @@
 package org.cykj.java;
 
-import org.junit.Test;
+
 
 public class JavaTest {
 
 	public static void main(String args[]) {
 		System.out.println("hello world!");
+		System.out.println("我是范帅,我已经上传成功了，耶耶耶！！");
 	}
 }
