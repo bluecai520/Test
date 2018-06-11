533acf5e010b971345486bfddf8680c2b734367f
JianSiYuan <839866596@qq.com>
2018-06-11 22:11:37 +0800

    添加一行

--- src/org/cykj/java/JavaTest.java
+++ src/org/cykj/java/JavaTest.java
@@ -8,6 +8,6 @@ public class JavaTest {
 		System.out.println("hello world!");
 		System.out.println("鎴戞槸鑼冨竻,鎴戝凡缁忎笂浼犳垚鍔熶簡锛岃�惰�惰�讹紒锛�");
 		System.out.println("Hello boys ,I am MaoCong");
-		
+		System.out.println("jian");
 	}
 }
