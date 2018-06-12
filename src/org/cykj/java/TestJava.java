f4a51768814ed0d36f42636ee1d941cebc3d8e2d
1152179588@qq.com <Viking@DESKTOP-USC05O6>
2018-06-12 15:16:13 +0800

    15:15此时重复问题

---
+++ src/org/cykj/java/TestJava.java
@@ -0,0 +1,17 @@
+package org.cykj.java;
+
+public class TestJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("哈哈哈哈");
+		System.out.println("春哥修改此函数");
+		
+	}
+	
+	public void chungeTest() {
+		System.out.println("15:15此时重复问题");
+	}
+
+}
