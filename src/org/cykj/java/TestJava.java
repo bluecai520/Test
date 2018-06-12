85ef0a8bcb95e18dbef1a4e5fe733b7ae2fa2d2f
1152179588@qq.com <Viking@DESKTOP-USC05O6>
2018-06-12 15:44:59 +0800

    测试

--- src/org/cykj/java/TestJava.java
+++ src/org/cykj/java/TestJava.java
@@ -7,10 +7,13 @@ public class TestJava {
 
 		System.out.println("哈哈哈哈");
 		System.out.println("春哥修改此函数");
-		
 	}
+
 	public void addMehtod() {
 		System.out.println("添加....");
 	}
-
+	
+	public void chungeTest() {
+		System.out.println("15:15此时重复问题");
+	}
 }
